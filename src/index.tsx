edb85551677a825cdb6e50833a10bdfb1821d3de
mdirshaddev <mdirshaddev@gmail.com>
2022-06-27 23:00:20 +0530

    🧑‍💻 React Root App Component updated
    * Sentry integration with React App

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,25 @@ import ReactDOM from 'react-dom/client';
 import App from 'App';
 import * as serviceWorkerRegistration from 'utils/serviceWorkerRegistration';
 import reportWebVitals from 'utils/reportWebVitals';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+  beforeSend(event, hint) {
+    // Check if it is an exception, and if so, show the report dialog
+    if (event.exception) {
+      Sentry.showReportDialog({ eventId: event.event_id });
+    }
+    return event;
+  }
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
