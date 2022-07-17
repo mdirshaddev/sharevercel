20fafb67e2b075b6fb34622de2b32e622d1342fb
mdirshaddev <mdirshaddev@gmail.com>
2022-07-17 20:52:43 +0530

    terraform: 🔑 Environemtn variables update for vercel provider
    * ✅ tested from local environment

--- vercel-terraform.tf
+++ vercel-terraform.tf
@@ -1,3 +1,39 @@
+variable "REACT_APP_SENTRY_DSN" {
+  type = string
+}
+
+variable "REACT_APP_API_KEY" {
+  type = string
+}
+
+variable "REACT_APP_AUTH_DOMAIN" {
+  type = string
+}
+
+variable "REACT_APP_DATABASE_URL" {
+  type = string
+}
+
+variable "REACT_APP_PROJECT_ID" {
+  type = string
+}
+
+variable "REACT_APP_STORAGE_BUCKET" {
+  type = string
+}
+
+variable "REACT_APP_MESSAGING_SENDER_ID" {
+  type = string
+}
+
+variable "REACT_APP_APP_ID" {
+  type = string
+}
+
+variable "REACT_APP_MEASUREMENT_ID" {
+  type = string
+}
+
 terraform {
   required_providers {
     vercel = {
@@ -10,6 +46,54 @@ terraform {
 resource "vercel_project" "sharemem" {
   name      = "sharemem"
   framework = "nextjs"
+  environment = [
+        {
+      key = "REACT_APP_SENTRY_DSN"
+      value = var.REACT_APP_SENTRY_DSN
+      target = ["production", "development"]
+    },
+
+    {
+      key = "REACT_APP_API_KEY"
+      value = var.REACT_APP_API_KEY
+      target = ["production", "development"]
+    },
+        {
+      key = "REACT_APP_AUTH_DOMAIN"
+      value = var.REACT_APP_AUTH_DOMAIN
+      target = ["production", "development"]
+    },
+        {
+      key = "REACT_APP_DATABASE_URL"
+      value = var.REACT_APP_DATABASE_URL
+      target = ["production", "development"]
+    },
+        {
+      key = "REACT_APP_PROJECT_ID"
+      value = var.REACT_APP_PROJECT_ID
+      target = ["production", "development"]
+    },
+        {
+      key = "REACT_APP_STORAGE_BUCKET"
+      value = var.REACT_APP_STORAGE_BUCKET
+      target = ["production", "development"]
+    },
+        {
+      key = "REACT_APP_MESSAGING_SENDER_ID"
+      value = var.REACT_APP_MESSAGING_SENDER_ID
+      target = ["production", "development"]
+    },
+    {
+      key: "REACT_APP_APP_ID",
+      value: var.REACT_APP_APP_ID
+      target=["production", "development"]
+    },
+    {
+      key="REACT_APP_MEASUREMENT_ID",
+      value: var.REACT_APP_MEASUREMENT_ID
+      target=["production", "development"]
+    }
+  ]
   git_repository = {
     type = "github"
     repo = "mdirshaddev/sharemem"
